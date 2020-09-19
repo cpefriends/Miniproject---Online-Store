78bed4c141a238b59954a1bf3c94763988dad2cb
bryanimon <59811208+bryanimon@users.noreply.github.com>
2020-09-19 11:46:51 +0800

    Add all tables Remove products.sql, users.sql

--- src/main/resources/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-  `USER_ID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `NAME` varchar(255) NOT NULL,
-  `PASSWORD` varchar(255) NOT NULL,
-  `ADDRESS` varchar(255) NOT NULL,
-  `EMAIL` varchar(255) NOT NULL,
-  `TOTAL_ORDERS` int(11) NOT NULL)
